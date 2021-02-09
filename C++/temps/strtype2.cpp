5ae375a5ec4804a2d135ad4eae3bd8f5271dadcd
Blank <totalblank@yahoo.com>
2021-02-09 08:46:29 +0600

    Added files to temps/ folder

---
+++ C++/temps/strtype2.cpp
@@ -0,0 +1,35 @@
+// strtype2.cpp -- assigning, adding, and appending
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+#define ll long long
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+	string s1 = "penguin";
+	string s2, s3;
+
+	cout << "You can assign one string object to another: s2 = s1\n";
+	s2 = s1;
+	cout << "s1: " << s1 << ", s3: " << s2 << endl;
+	cout << "You can assign a C-style string to a string object.\n";
+	cout << "s2 = \"buzzard\"\n";
+	s2 = "buzzard";
+	cout << "s2: " << s2 << endl;
+	cout << "You can concatenate strings: s3 = s1 + s2\n";
+	s3 = s1 + s2;
+	cout << "s3: " << s3 << endl;
+	cout << "You can append strings.\n";
+	s1 += s2;
+	cout << "s1 += s2 yields s1 = " << s1 << endl;
+	s2 += " for a day";
+	cout << "s2 += \" for a day\" yields s2 = " << s2 << endl;
+
+	return 0;
+}
+
